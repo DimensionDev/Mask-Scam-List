a0741ed0337ef02c2a76b4b41e0570e55d66e498
Lantt <laanfor@gmail.com>
2022-11-08 19:02:20 +0800

    feat: create bloom filter to improve

--- script/index.ts
+++ script/index.ts
@@ -1,15 +1,15 @@
 import axios from 'axios'
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import { uniq } from 'lodash'
+import { ScalableBloomFilter } from 'bloom-filters'
 
-const DATA_URL = 'https://api.cryptoscamdb.org/v1/scams'
-const CDN_DATA_URL = 'some link'
+export const DATA_URL = 'https://api.cryptoscamdb.org/v1/scams'
 
 export type Response = {
   success: boolean
   result: Array<Site>
 }
+
 interface Site {
   id: string
   name: string
@@ -20,48 +20,22 @@ interface Site {
   description: string
   reporter: string
 }
-
-interface OnlineSiteInfo {
-  name: string
-  url: string
-  path: string
-  category?: number
-  subcategory?: number
-  description: string
-}
-
-interface OnlineCategory {
-  id: number
-  value: string
-}
-
 // @ts-ignore
 const outputDir = path.join(process.env.PWD, 'providers/cryptoscan-db')
 
-async function writeWebsiteToFile(data: OnlineSiteInfo[]) {
-  await fs.writeFile(path.join(outputDir, `list.json`), JSON.stringify(data), {
+export async function writeSingleWebsiteToFile(website: string, data: Site) {
+  await fs.writeFile(path.join(outputDir, `${website?.toLowerCase()}.json`), JSON.stringify(data), {
     encoding: 'utf-8',
   })
 }
 
-async function writeCategoriesToFile(key: 'categories' | 'sub-categories', data: OnlineCategory[]) {
-  await fs.writeFile(path.join(outputDir, `${key}.json`), JSON.stringify(data), {
+export async function writeFilterToFile(data: Site) {
+  await fs.writeFile(path.join(outputDir, 'filter', 'config.json'), JSON.stringify(data), {
     encoding: 'utf-8',
   })
 }
 
-async function getCategories(key: 'categories' | 'sub-categories', list: string[]): Promise<OnlineCategory[]> {
-  // const online = await axios.get<{ id: number; value: string }[]>(urlcat(CDN_DATA_URL, key))
-  const onlineCategories: OnlineCategory[] = []
-  const newCategories = list.filter((x) => !onlineCategories.find((c) => x === c.value))
-  const newList = uniq(newCategories)
-    .filter(Boolean)
-    .map((x, i) => ({ id: i + onlineCategories.length, value: x } as OnlineCategory))
-
-  return [...onlineCategories, ...newList]
-}
-
-async function main() {
+export async function main() {
   const result = await axios.get<Response>(DATA_URL)
   if (!result.data.success) {
     console.log('Fetch db data from API failed!')
@@ -69,31 +43,24 @@ async function main() {
   }
 
   const list = result.data.result
+  const filter = new ScalableBloomFilter(list.length)
 
-  const categories = await getCategories(
-    'categories',
-    list.map((x) => x.category),
-  )
-  const subCategories = await getCategories(
-    'sub-categories',
-    list.map((x) => x.subcategory),
-  )
+  for (const site of list) {
+    filter.add(site.name)
+    await writeSingleWebsiteToFile(site.name, site)
+  }
 
-  const sites = list.map((x) => {
-    const category = categories.find((c) => c.value === x.category)
-    const subCategory = subCategories.find((c) => c.value === x.subcategory)
-    return {
-      name: x.name,
-      url: x.url,
-      path: x.path,
-      category: category?.id,
-      subcategory: subCategory?.id,
-      description: x.description,
-    }
-  })
-  await writeWebsiteToFile(sites)
-  await writeCategoriesToFile('categories', categories)
-  await writeCategoriesToFile('sub-categories', subCategories)
+  const filterConfig = filter.saveAsJSON()
+  await writeFilterToFile(filterConfig)
+
+  // Make sure bloom filter is work
+  const content = await fs.readFile(path.join(outputDir, 'filter', 'config.json'), { encoding: 'utf-8' })
+  const contentJSON = JSON.parse(content)
+  const importedFilter = ScalableBloomFilter.fromJSON(contentJSON)
+
+  for (const item of list) {
+    if (!importedFilter.has(item.name)) throw new Error('Bloom Filter create failed')
+  }
 }
 
 main()
